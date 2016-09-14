fcf844b4847b2ef7b91a38a46a7b42d5ed79a3b0
kmehkeri <kmehkeri@gmail.com>
2016-09-14 13:42:37 +0200

    Handle server termination

--- src/main/scala/HelloAkka.scala
+++ src/main/scala/HelloAkka.scala
@@ -5,9 +5,12 @@ import akka.http.scaladsl.server.Directives._
 
 object HelloAkka {
   def main(args: Array[String]) {
+    val host = "localhost"
+    val port = 8080
 
     implicit val actorSystem = ActorSystem("hello-system")
     implicit val actorMaterializer = ActorMaterializer()
+    implicit val executionContext = actorSystem.dispatcher
 
     val route = pathSingleSlash {
       get {
@@ -17,7 +20,13 @@ object HelloAkka {
       }
     }
 
-    Http().bindAndHandle(route, "localhost", 8080)
-    println("Server started at localhost:8080")
+    val bindingFuture = Http().bindAndHandle(route, host, port)
+    println(s"Server started at ${host}:${port}\nPress ENTER to stop...")
+    readLine()
+
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => actorSystem.terminate()) // and shutdown when done
   }
 }
+
